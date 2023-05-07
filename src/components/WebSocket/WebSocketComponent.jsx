3782078c0fe31bddc199eb9145755e61a618a606
neo1202 <neowuhuayo@gmail.com>
2023-05-07 18:52:40 +0800

    feat:  chatroom version1 0507

--- src/components/WebSocket/WebSocketComponent.jsx
+++ src/components/WebSocket/WebSocketComponent.jsx
@@ -89,8 +89,9 @@ function WebSocketComponent({ rid, rname }) {
     }, [messageHistory]);
 
     return (
-        <div className="container mx-auto py-4 border-2">
-            <div className="mx-auto max-w-lg">
+        <>
+        <div className="container mx-auto px-4 py-4 border-2 ">
+            <div className="mx-auto max-w-xl h-full relative rounded-lg ">
                 {messageHistory.map((message, index) => (
                     <div key={message.id} className="my-2 flex justify-between">
                         <div className="font-bold">
@@ -101,29 +102,70 @@ function WebSocketComponent({ rid, rname }) {
                     </div>
                 ))}
                 <div ref={messageEndRef} />
-                <form
-                    className="flex justify-between"
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        handleSendMessage(e.target.content.value);
-                        e.target.reset();
-                    }}
-                >
-                    <input
-                        className="mr-2 w-2/3 rounded border border-gray-300 p-2 focus:border-blue-500 focus:outline-none"
-                        id="content"
-                        placeholder="Message"
-                        type="text"
-                    />
-                    <button
-                        className="rounded bg-blue-500 p-2 text-white hover:bg-blue-600"
-                        type="submit"
+
+                <div className="flex flex-row items-center rounded-xl w-full h-14 bg-zinc-200 px-4
+                                absolute bottom-0 left-0">
+                    <form
+                        className="flex w-full"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            handleSendMessage(e.target.content.value);
+                            e.target.reset();
+                        }}
                     >
-                        Send
-                    </button>
-                </form>
+                        <button className="flex flex-none items-center justify-center text-gray-400 hover:text-gray-600">
+                            <svg
+                            className="w-5 h-5"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                            >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                            ></path>
+                            </svg>
+                        </button>
+                        <input
+                            className="shrink mr-2 w-3/4 rounded border border-gray-300 p-2 ml-4 focus:border-blue-500 focus:outline-none"
+                            id="content"
+                            placeholder="Message"
+                            type="text"
+                        />
+                        <div className="flex-none ml-4 flex items-center">
+                            <button
+                                className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
+                                type="submit"
+                            >
+                                <span>Send</span>
+                                <span className="ml-2">
+                                <svg
+                                    className="w-4 h-4 transform rotate-45 -mt-px"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                                    ></path>
+                                </svg>
+                                </span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
+
+            
         </div>
+        </>
     );
 }
 
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -36,18 +36,39 @@ function ChatPage() {
         <>
         <div className="h-screen w-screen bg-[url('https://source.unsplash.com/OTy0mkqc2Yk')] bg-no-repeat bg-cover bg-center bg-fixed -z-50 top-0 right-0  absolute"></div>
         <div className="h-screen w-screen bg-[url('https://source.unsplash.com/OTy0mkqc2Yk')] bg-no-repeat bg-cover bg-center bg-fixed -z-50 top-0 left-0   absolute"></div>
-        <div className="container relative h-[660px] w-screen">
-            <div className="flex flex-row ml-32 mr-20 w-full h-full backdrop-blur-sm">
+        <div className="w-screen min-w-min-content relative h-[640px]">
+            <div className="flex flex-row h-full backdrop-blur-sm ml-[15%] mr-[12%] ">
 
-                <div className="mx-auto flex flex-col w-56 border-2 border-r-0">
-                    <div className="mb-4 text-2xl font-bold pt-2 pl-2">Receivers</div>
+                <div className="mx-auto flex flex-col w-56 border-2 border-r-0 ">
+
+                    <div className="flex w-full pt-2 mb-4 item-center justify-center">
+                        <div className="flex items-center justify-center rounded-2xl text-zinc-700 bg-lime-200/30 h-10 w-10 inline">
+                            <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                            >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+                            ></path>
+                            </svg>
+                        </div>
+                        <div className="text-2xl font-bold pt-2 pl-px ml-1">Receivers</div>
+                    </div>
                     <ul>
                         {messageUsers && messageUsers.map((messageUser) => (
                             <li key={messageUser.id}>
                                 <button
                                     className={classNames(
-                                        "w-full px-4 py-2 text-left",
-                                        selectedReceiver === messageUser.id ? "bg-gray-300" : "bg-white",
+                                        "w-11/12 px-4 py-2 text-left border-2 mb-1 mx-1.5",
+                                        selectedReceiver === messageUser.id 
+                                            ? "bg-gray-300 ring ring-zinc-500/50 hover:scale-105" 
+                                            : "bg-white hover:scale-105",
                                     )}
                                     type="button"
                                     onClick={() => handleReceiverChange(messageUser.id)}
@@ -62,6 +83,8 @@ function ChatPage() {
 
             </div>
         </div>
+
+
         </>
     );
     
